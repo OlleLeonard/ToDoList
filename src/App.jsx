00204313338682c45f6281f3ea53e10ecdb5483d
OlleLeonard <olleleonard@gmail.com>
2024-04-24 16:06:25 +0200

    ToDo typ färdig

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,39 @@ function App() {
 const [list, setList] = useState([]);
 const [input, setInput] = useState ("");
 
+const addTodo = (todo) => {
+  const newTodo = {
+    id: Math.random(),
+    todo: todo
+  }
+
+  setList([...list,newTodo]);
+
+  setInput("");
+};
+
+const deleateTodo = (id) => {
+  const newList = list.filter((todo) => todo.id !== id);
+  setList(newList);
+};
+
   return (
     <div>
       <h1>ToDo List</h1>
       <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-      <button>Lägg till i listan</button>
+      <button onClick={() => addTodo(input)}>Lägg till i listan</button>
+
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+          {todo.todo}
+          <button onClick={() => deleateTodo(todo.id)}>X</button>
+          </li>
+        ))}
+      </ul>
+
     </div>
-  )
+  );
 }
 
 export default App
